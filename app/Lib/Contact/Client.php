92aec0fe7da990f792b999393ecf025dd482c6e4
Davey <davey.velthove@shifft.com>
2017-11-19 13:05:16 +0100

    Add Client

--- app/Lib/Contact/Client.php
+++ app/Lib/Contact/Client.php
@@ -2,3 +2,46 @@
 
 namespace App\Lib\Contact;
 
+final class Client
+{
+    /**
+     * Hold the listening socket for this instance.
+     *
+     * @var Socket
+     */
+    private $socket;
+
+    /**
+     * Buffer to send messages to the client.
+     *
+     * @var array
+     */
+    private $message = [];
+
+    /**
+     * Buffer with data to process by the server.
+     *
+     * @var array
+     */
+    private $read = [];
+
+    /**
+     * Constructor for this class.
+     *
+     * @param $socket Socket
+     */
+    public function __construct($socket = null)
+    {
+        if (!$socket)
+        {
+            return;
+        }
+
+        $this->socket = $socket;
+    }
+
+    private function listen()
+    {
+        // ?
+    }
+}
